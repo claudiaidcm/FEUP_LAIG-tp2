6aa05561a3ff9d3b15b7478b335d26f1dc3f52e4
vitorsly <vitorsly@sapo.pt>
2019-11-11 16:00:59 +0000

    KeyframeAnimation almost done

--- KeyframeAnimation.js
+++ KeyframeAnimation.js
@@ -1,40 +1,62 @@
 class KeyframeAnimation extends Animation {
-  constructor(id, scene, time, animations) {
+  constructor(id, scene, animations) {
+    super(scene);
     this.id = id;
     this.scene = scene;
-    this.time = time;
     this.animations = animations;
-    this.animID = 0;
-    this.maxTime = 0;
-    this.timeLastAnim = 0;
+    this.instants = [];
+    this.maxTime = animations[animations.length-1].time;
+    this.currentKeyframe = 0;
+    this.oldTime = -1;
+    this.firstTime = -1;
     this.matrix = mat4.create();
 
-    for (var i = 0; i < animations.length; i++) {
-      this.maxTime += this.scene.graph.animations[animations[i]].totalTime;
+    this.testi = 0;
+
+    for (var i = 0; i < this.animations.length; i++) {
+       this.instants.push(animations[i].time);
     }
 
   };
 
   update(deltaTime) {
     mat4.identity(this.matrix);
+    
+    
+     this.testi++;
 
-    var currentAnimation = this.scene.graph.animations[this.animations[this.animID]];
+    if(this.oldTime==-1){
+     this.oldTime=deltaTime;
+     this.firstTime = deltaTime;
+     return;
+   }
 
-    if ((deltaTime - this.timeLastAnim) > currentAnimation.totalTime) {
-      this.animID++;
-      this.timeLastAnim += currentAnimation.totalTime;
-    } else {
-      currentAnimation.update(deltaTime);
-      this.matrix = currentAnimation.matrix;
+   var intervalTime=deltaTime-this.lastTime;
+    
+    this.lastTime=deltaTime;
+    
+     if (this.testi % 9715 == 0) {
+      console.log("Updating Animation " + this.id + ". Current Keyframe: " + this.currentKeyframe + ". It's been " + (deltaTime - this.firstTime)/1000 + " seconds since the program has started");      console.log("Last Interval:" + intervalTime);
     }
+
+    if(this.currentKeyframe == this.animations.length) return;
+
+    if((deltaTime-this.firstTime)/1000 > this.instants[this.currentKeyframe]) {
+      this.currentKeyframe++;
+    } 
+   
+
   };
 
   apply() {
     var transform = mat4.create();
     mat4.identity(transform);
+    
+    //Apply Transformation Matrix(s)
+    //console.log("Applying Animation " + this.id);
+    
 
-
-    fromRotationTranslationScale(transform, q, v, s)
+    
   };
 
 };
